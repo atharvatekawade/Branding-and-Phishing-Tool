7519fc60bb54be75db0e8ed12a9bdb5da14b7e24
Atharva Tekawade <atharvatekawade@gmail.com>
2022-12-06 13:03:46 +0530

    vercel error cleared

--- apis/apis/settings.py
+++ apis/apis/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
--- apis/build_files.sh
+++ apis/build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python3.11  -m pip install -r requirements.txt
-python3.11 manage.py collectstatic  --noinput --clear
+python3.11.0  -m pip install -r requirements.txt
+python3.11.0 manage.py collectstatic  --noinput --clear
 echo " BUILD END"
