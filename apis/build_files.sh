3baf04733575508188f4edf956042f328b9e1ffc
Atharva Tekawade <atharvatekawade@gmail.com>
2022-12-06 13:17:44 +0530

    vercel error cleared

--- apis/build_files.sh
+++ apis/build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python3.11.0  -m pip install -r requirements.txt
-python3.11.0 manage.py collectstatic  --noinput --clear
+python  -m pip install -r requirements.txt
+python manage.py collectstatic  --noinput --clear
 echo " BUILD END"
